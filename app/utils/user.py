ed054ff1c0aec9137b3032c3f68b32aebeea3f13
Zehan <zehan.kareem@gmail.com>
2024-01-23 23:40:34 +0600

    Some design changes

--- app/utils/user.py
+++ app/utils/user.py
@@ -4,14 +4,19 @@ from utils.blogs import Blog
 from email_validator import validate_email
 
 class User:
+    def get_headers(access_token):
+        return {"Authorization": f"Bearer {access_token}"}
+    
     def login(API_URL):
         with st.form("Login form",clear_on_submit=False):
             st.header("Login")
             form_data = {"username":None, 
                 "password": None}
             form_data["username"]=st.text_input("username")
-            form_data["password"]=st.text_input("password", type="password")
-            submit = st.form_submit_button("Login",use_container_width=True)
+            form_data["password"]=st.text_input("password",
+                                                type="password")
+            submit = st.form_submit_button("Login",
+                                           use_container_width=True)
             
             # Login button
             if submit:
@@ -49,61 +54,77 @@ class User:
                 st.error("Please enter valid password")
                   
     def show_profile(access_token, API_URL):
+        
+        def show_user_info(user_info):
+            
+            st.header("User Info")
+            st.divider()
+            
+            st.write(f"<b>Username:</b>",
+                     user_info["username"],
+                     unsafe_allow_html=True)
+            
+            st.write("<b>Name:</b>",
+                     user_info["full_name"],
+                     unsafe_allow_html=True)
+            
+            st.write("<b>Email:</b>",
+                     user_info["email"],
+                     unsafe_allow_html=True)
 
-        # API endpoint
-        api_endpoint = f"{API_URL}/user/me"  # replace with your actual API endpoint
+        api_endpoint = f"{API_URL}/user/me"
 
-        # Headers with the access token
-        headers = {"Authorization": f"Bearer {access_token}"}
+        # Getting the headers "Bearer <Token>"
+        headers = User.get_headers(access_token)
 
-        # Make GET request to the API endpoint
+        # Make GET request to the API endpoint for Profile
         response = requests.get(api_endpoint, headers=headers)
         user_info = response.json()
+        
         # Check if the request was successful
         if response.status_code == 200:
             
             with st.container(border=True):
-                col1, col2 = st.columns(2)
+                col1, _, col3 = st.columns([3, 3, 4])
                 with col1:
-                    st.image("https://cdn-icons-png.flaticon.com/512/1053/1053244.png", width=200)
-                with col2:
+                    st.image("https://cdn-icons-png.flaticon.com/512/1053/1053244.png", use_column_width=True)
+                    st.divider()
                     edit = st.button("✏️ Edit Profile", type="primary", use_container_width=True)
+                with col3:
                     if edit:
                         with st.form("Profile Edit Form", clear_on_submit=False):
                             username = st.text_input(label="Username: ", value=user_info["username"])
                             name = st.text_input(label="Name: ", value=user_info["full_name"])
                             email = st.text_input(label="Email: ", value=user_info["email"])
                             update = st.form_submit_button(label="💾 Update Profile", use_container_width=True, type="primary")
+                            if update:
+                                show_user_info(user_info)
+                                
                         cancel=st.button("Cancel", use_container_width=True)
-                    else:
-                        st.write("<b>Username:</b>", user_info["username"], unsafe_allow_html=True)
-                        st.write("<b>Name:</b>", user_info["full_name"], unsafe_allow_html=True)
-                        st.write("<b>Email:</b>", user_info["email"], unsafe_allow_html=True)
+                        
+                        if cancel:
+                            show_user_info(user_info)
                     
-        
+                    else:
+                        show_user_info(user_info)
+                        
+            # --- Adding Blog Post from Profile Page --- #
+            Blog.add_blog_post(headers=headers, API_URL=API_URL)
+            
+            # --- Navigating through different options in profile page --- #
+            with st.container(border=True):
+                col1, col2, col3, col4 = st.columns(4)
+                with col2:
+                    my_posts = st.button(":red[My Posts 📑]",use_container_width=True)
+                with col3:
+                    pictures = st.button("Pictures 🖼️", use_container_width=True)
+            
+            if my_posts:            
+                Blog.show_my_blogs(headers, API_URL)
+            if pictures:
+                st.error("No pictures yet")
+            
         elif response.status_code == 401:
             st.error("Unauthorized. Please login")
         else:
             st.error(f"Error: {response.status_code}")
-        with st.container(border=True):
-            col1, col2, col3 = st.columns(3)
-            with col1:
-                my_posts = st.button(":red[My Posts 📑]",use_container_width=True)
-            with col2:
-                pictures = st.button("Pictures 🖼️", use_container_width=True)
-            with col3:
-                update_profile=st.button("Edit Profile ✏️",type="primary", use_container_width=True)
-            
-        if my_posts:            
-            Blog.show_my_blogs(headers, API_URL)
-        if pictures:
-            st.error("No pictures yet")
-        if update_profile:
-            with st.form("Profile Edit Form", clear_on_submit=False):
-                username = st.text_input(label="Username: ", value=user_info["username"])
-                name = st.text_input(label="Name: ", value=user_info["full_name"])
-                email = st.text_input(label="Email: ", value=user_info["email"])
-                update = st.form_submit_button(label="Update Profile", use_container_width=True, type="primary")
-        else:
-            Blog.show_my_blogs(headers,API_URL)
-        
